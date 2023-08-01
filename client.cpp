630aeadc7426fa702f73101dcc88cebc9cf2246f
Frank Gao <frank.qixiang.gao@gmail.com>
2023-07-31 23:36:50 -0500

    add read_full and write_full

--- client.cpp
+++ client.cpp
@@ -39,3 +39,48 @@ int main() {
 
     return 0;
 }
+
+while (true) {
+    // accept
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+    if (connfd < 0) {
+        continue; // error
+    }
+
+    // only serves one client connectioon at once
+    while (true) {
+        int32_t err = one_request(connfd);
+        if (err) {
+            break;
+        }
+    }
+    close(connfd);
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv < 0) {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv < 0) {
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -4,54 +4,74 @@
 #include <sys/socket.h> // for socket(), bind(), and connect()
 #include <unistd.h>     // for close()
 
+// handle error cases and terminate the program
 void die(char *errorMessage) {
     // perror(errorMessage);
     exit(1);
 }
 
+// handle the communication with the client
 void do_something(int connfd){
+    // Buffer to hold data read from the client
     char rbuf[64] = {};
+
+    // Read data from client and handle error
     ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
     if(n < 0) {
         die("read()");
     }
 }
 
+
 int main() {
+    // Initialize variable
     int val = 1;
+
+    // create the socket and handle any error
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if(fd < 0) {
         die("socket()");
     }
 
+    // Set socket options and handle any error
     if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
         die("setsockopt()");
     }
 
+    // Initialize and set up the server address structure
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(1234);
     addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    // Bind the socket to the address and handle any error
     int rv = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
     if(rv < 0) {
         die("bind()");
     }
 
+    // Start listening on the socket and handle any error
     rv = listen(fd, SOMAXCONN);
     if(rv < 0) {
         die("listen()");
     }
 
+    // Accept and handle incoming connections indefinitely
     while(true){
         struct sockaddr_in client_addr = {};
         socklen_t socklen = sizeof(client_addr);
+
+        // Accept a client connection and handle any error
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
         if(connfd < 0) {
-            continue; // error
+            continue; // error,
         }
+
+        // Handle the client connection and then close it
         do_something(connfd);
         close(connfd);
     }
-    
+
     return 0;
 }
+
