705f2be947f9bf8f59166cda1a74b89d74bfeddc
Frank Gao <frank.qixiang.gao@gmail.com>
2023-08-12 23:35:06 -0500

    Aug 12 - 2

--- server.cpp
+++ server.cpp
@@ -105,11 +105,97 @@ static int32_t one_request(int connfd) {
     return write_all(connfd, wbuf, 4 + len);
 }
 
+static void fd_set_nb(int fd) {
+    errno = 0;
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (errno) {
+        die("fcntl error");
+        return;
+    }
+
+    flags |= O_NONBLOCK;
+
+    errno = 0;
+    (void)fcntl(fd, F_SETFL, flags);
+    if (errno) {
+        die("fcntl error");
+    }
+}
+
+enum {
+    STATE_REQ = 0,
+    STATE_RES = 1,
+    STATE_END = 2,
+};
+
+struct Conn {
+    int fd = -1;
+    uint32_t state = 0 // either STATE_REQ or STATE_RES
+    size_t rbuf_size = 0;
+    uint8_t rbuf[4 + k_max_msg + 1];
+    // buffer for writing
+    size_t wbuf_size = 0;
+    size_t wbuf_sent = 0;
+    uint8_t wbuf[4 + k_max_msg + 1];
+}
+
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
     }
+
+
+    // a map of all client connections, keyed by fd
+    std::vector<Conn *> fd2conn;
+
+    // set the listen fd to nonblocking mode
+    fd_set_nb(fd);
+
+    // the event loop
+    std::vector<struct pollfd> poll_args;
+
+    while(true){
+        poll_args.clear();
+        struct pollfd pfd = {fd, POLLIN, 0};
+        poll_args.push_back(pfd);
+        // connection fds
+        for (Conn *conn : fd2conn) {
+            if (!conn) {
+                continue;
+            }
+            struct pollfd pfd = {};
+            pfd.fd = conn->fd;
+            pfd.events = (conn->state == STATE_REQ) ? POLLIN : POLLOUT;
+            pfd.events = pfd.events | POLLERR;
+            poll_args.push_back(pfd);
+        }
+
+        //poll for active fds
+        int rv = poll(poll_args.data(), poll_args.size(), 1000);
+        if (rv < 0) {
+            die("poll()");
+        }
+
+        // process active connections
+        for (size_t i = 1, i < poll_args.size(); ++i) {
+            if (poll_args[i].revents) {
+                Conn *conn = fd2conn[poll_args[i].fd];
+                connection_io(conn);
+                if (conn->state == STATE_END) {
+                    fd2conn[conn->fd] = NULL;
+                    (void)close(conn->fd);
+                    free(conn);
+                }
+            }
+        }
+
+        // try to accept a new connection if the listening fd is active
+        if (poll_args[0].revents) {
+            (void)accept_new_conn(fd2conn, fd);
+        }
+    }
+
 
     // this is needed for most server applications
     int val = 1;
@@ -151,39 +237,5 @@ int main() {
     }
 
     return 0;
-}
-
-// static void fd_set_nb(int fd) {
-//     errno = 0;
-//     int flags = fcntl(fd, F_GETFL, 0);
-//     if (errno) {
-//         die("fcntl error");
-//         return;
-//     }
-
-//     flags |= O_NONBLOCK;
-
-//     errno = 0;
-//     (void)fcntl(fd, F_SETFL, flags);
-//     if (errno) {
-//         die("fcntl error");
-//     }
-// }
-
-enum {
-    STATE_REQ = 0,
-    STATE_RES = 1,
-    STATE_END = 2,
-};
-
-struct Conn {
-    int fd = -1;
-    uint32_t state = 0 // either STATE_REQ or STATE_RES
-    size_t rbuf_size = 0;
-    uint8_t rbuf[4 + k_max_msg + 1];
-    // buffer for writing
-    size_t wbuf_size = 0;
-    size_t wbuf_sent = 0;
-    uint8_t wbuf[4 + k_max_msg + 1];
 }
 
